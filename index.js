f6da6a3095be7d38e2fbe6947c546f4c2f0dc451
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-16 12:16:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const nn = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello nn`}))
+  res.end()
+  
+}
+
+export default nn
